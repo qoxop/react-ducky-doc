c2355e257ca231c7dfcb6dfa9f3ac14ced9ea0d5
张集伟 <zhangjw01@mingyuanyun.com>
2021-10-31 12:44:38 +0800

    editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/qoxop/react-ducky-doc/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            'https://github.com/qoxop/react-ducky-doc/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
